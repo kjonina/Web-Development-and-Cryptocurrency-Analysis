6eedd554473b944b319086b7ff05aaf6abe66906
kjonina <kjonina@gmail.com>
2021-07-07 15:56:52 +0100

    Trying to intergrate the yahoo table

--- karina/thesis/models.py
+++ karina/thesis/models.py
@@ -2,7 +2,4 @@
 from django.db import models
 
 class Thesis(models.Model):
-    title = models.CharField(max_length=255)
-    pub_date = models.DateTimeField()
-    body = models.TextField(max_length=1500)
-    image = models.ImageField(upload_to='images/')
+    crypto = models.CharField(max_length=255)
