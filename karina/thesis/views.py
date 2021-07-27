a4a655165f9528120a8382f88c180da93ce400e7
kjonina <kjonina@gmail.com>
2021-07-27 14:41:51 +0100

    IT WORKS!!!!

--- karina/thesis/views.py
+++ karina/thesis/views.py
@@ -36,98 +36,56 @@ from thesis.services.get_crypto_name import get_crypto_name
 def thesis(request):
     df_cryptolist, json_three = get_yahoo_table(request)
 
-    cryptolist = []
+    if request.method == 'GET':
+        # getting the user's input
+        crypto = request.GET.get('crypto_ticket')
+        crypto_ticket = str(crypto).upper()
+        print(crypto_ticket)
 
-    index = 0
+        cryptolist = []
 
-    while index < len(df_cryptolist.iloc[:,0]):
-        try:
-            for crypto in df_cryptolist.iloc[:,0]:
-                cryptolist.append(str(crypto))
-                index += 1
+        index = 0
 
-        except:
-            index = len(df_cryptolist.iloc[:,0])
-            break
-        print(cryptolist)
+        while index < len(df_cryptolist.iloc[:,0]):
+            try:
+                for crypto in df_cryptolist.iloc[:,0]:
+                    cryptolist.append(str(crypto))
+                    index += 1
 
-    # getting the user's input
-    crypto = request.GET.get('crypto_ticket')
-    crypto_ticket = str(crypto).upper()
-    print(crypto_ticket)
+            except:
+                index = len(df_cryptolist.iloc[:,0])
+                break
+            print(cryptolist)
+
+        if crypto_ticket == 'NONE':
+            print('Please select a cryptocurrency.')
+            return render(request, 'thesis/thesis_home.html', {'tablesinfo': json.loads(json_three), 'error':'Please select a cryptocurrency.'})
 
 
-    if not crypto_ticket in cryptolist:
-        print('Sorry. You did not select an available symbol or you misspelled the symbol')
-        return render(request, 'thesis/thesis_home.html', {'tablesinfo': json.loads(json_three), 'error':'Sorry. You did not select an available symbol or you misspelled the symbol'})
+        elif crypto_ticket in cryptolist:
+            # print('You have selected: ', crypto_ticket)
+
+            crypto_name = get_crypto_name(request, crypto_ticket)
+            # print(crypto_name)
+
+            # creating the df dataset
+            df = create_df(request,  crypto_ticket, crypto_name)
+            # print(df)
+
+            # creating the df dataset
+            y = create_y(request,  crypto_ticket, crypto_name)
+            # print(y)
+
+            return render(request, 'thesis/thesis_home.html', {'error':'You have selected: {}'.format(str(crypto_ticket)),
+                'get_crypto_info': json.loads(get_crypto_info(request, crypto_ticket)),
+                'tablesinfo': json.loads(json_three),
+                'price_sma_volume_chart': price_sma_volume_chart(request, df, crypto_name),
+                'death_cross': death_cross(request, df, crypto_name),
+                'hist_box_pct_change': hist_box_pct_change(request, y, crypto_name),
+                'rolling_m_sd': rolling_m_sd(request, y, crypto_name)})
+        else:
+            # print('Sorry. You did not select an available symbol or you misspelled the symbol')
+            return render(request, 'thesis/thesis_home.html', {'tablesinfo': json.loads(json_three), 'error':'Sorry. You did not select an available symbol or you misspelled the symbol'})
 
     else:
-        print('You have selected: ', crypto_ticket)
-
-        crypto_name = get_crypto_name(request, crypto_ticket)
-        print(crypto_name)
-
-        # creating the df dataset
-        df = create_df(request,  crypto_ticket, crypto_name)
-        #print(df)
-
-        # creating the df dataset
-        y = create_y(request,  crypto_ticket, crypto_name)
-        #print(y)
-
-        df_cryptolist, json_three = get_yahoo_table(request)
-
-        return render(request, 'thesis/thesis_home.html', {'error':'You have selected: {}'.format(str(crypto_ticket)),
-            'get_crypto_info': json.loads(get_crypto_info(request, crypto_ticket)),
-            'tablesinfo': json.loads(json_three),
-            'price_sma_volume_chart': price_sma_volume_chart(request, df, crypto_name),
-            'death_cross': death_cross(request, df, crypto_name),
-            'hist_box_pct_change': hist_box_pct_change(request, y, crypto_name),
-            'rolling_m_sd': rolling_m_sd(request, y, crypto_name)})
-
-
-
-# def thesis(request):
-#     df_cryptolist, json_three = get_yahoo_table(request)
-#
-#     cryptolist = []
-#
-#     index = 0
-#
-#     while index < len(df_cryptolist.iloc[:,0]):
-#         try:
-#             for crypto in df_cryptolist.iloc[:,0]:
-#                 cryptolist.append(str(crypto))
-#                 index += 1
-#
-#         except:
-#             index = len(df_cryptolist.iloc[:,0])
-#             break
-#         print(cryptolist)
-#     # getting the user's input
-#     crypto = request.GET.get('crypto_ticket')
-#     crypto_ticket = str(crypto).upper()
-#     print(crypto_ticket)
-#
-#     print('You have selected: ', crypto_ticket)
-#
-#     crypto_name = get_crypto_name(request, 'BTC-USD')
-#     print(crypto_name)
-#
-#     # creating the df dataset
-#     df = create_df(request,  'BTC-USD', crypto_name)
-#     #print(df)
-#
-#     # creating the df dataset
-#     y = create_y(request,  'BTC-USD', crypto_name)
-#     #print(y)
-#
-#     df_cryptolist, json_three = get_yahoo_table(request)
-#
-#     return render(request, 'thesis/thesis_home.html', {'error':'You have selected: {}'.format(str(crypto_ticket)),
-#     'get_crypto_info': json.loads(get_crypto_info(request, 'BTC-USD')),
-#     'tablesinfo': json.loads(json_three),
-#     'price_sma_volume_chart': price_sma_volume_chart(request, df, crypto_name),
-#     'death_cross': death_cross(request, df, crypto_name),
-#     'hist_box_pct_change': hist_box_pct_change(request, y, crypto_name),
-#     'rolling_m_sd': rolling_m_sd(request, y, crypto_name)})
+        return render(request, 'thesis/thesis_home.html')
