6d9e9d4e0e35d50000c38d92e70820e3f5995c60
kjonina <kjonina@gmail.com>
2021-07-27 12:20:24 +0100

    Adding personal achievement to the website

--- karina/achievements/models.py
+++ karina/achievements/models.py
@@ -3,9 +3,13 @@ from django.db import models
 
 class Achievement(models.Model):
     image = models.ImageField(upload_to='images/')
-    title = models.CharField(max_length=200)
+    pub_date = models.DateTimeField()
+    title = models.CharField(max_length=50)
     summary = models.CharField(max_length=200)
 
 
     def __str__(self):
         return self.title
+
+    def pub_date_pretty(self):
+        return self.pub_date.strftime('%e %b %Y')
